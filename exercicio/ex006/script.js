a7102b47f27acaba8be730917ff6a9d1b919d22d
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-02-07 21:12:44 -0300

    acabou ;-;

--- exercicio/ex006/script.js
+++ exercicio/ex006/script.js
@@ -1,3 +1,18 @@
 function carregar() {
+let message = document.getElementById('message')
+let photo = document.getElementById('picture')
+let date = new Date()
+let hour = date.getHours()
+message.innerHTML = `<h2>Agora são ${hour} `
+if (hour > 0 && hour < 12){
+    picture.src = 'manha.jpg'
+    document.body.style.backgroundColor
+}else if (hour >=12 && hour < 18 ){
+    picture.src = 'tarde.jpg'
+    document.body.style.backgroundColor = '#b88423'
+}else{
+    picture.src = 'noite.jpg'
+    document.body.style.backgroundColor ='#10111e'
+}
 
 }

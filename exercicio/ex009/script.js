259ff20fb18db0a63787a076bc73c64bae6b16a8
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-02-12 16:52:25 -0300

    erro
    ta com erro, tenho que resolver

--- exercicio/ex009/script.js
+++ exercicio/ex009/script.js
@@ -1,3 +1,11 @@
-function carregar() {
-    
+function analisar(){
+    var data = new Date
+    var ano = data.getFullYear
+    var Nome = document.getElementById('nome')
+    var AnoNascimento = document.getElementById('AnoNascimento')
+    var Sexo = document.getElementsByTagNameNS('sexo')
+    var resultado = getElementById('resultado')
+    if (AnoNascimento.value.length == 0 || AnoNascimento.value > ano || Nome.length == 0){
+        window.alert('alguma informação está errada')
+    }
 }

9f380d69b4a8d6c6613e93366e9b7b5a56b6a2e3
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-02-12 18:52:54 -0300

    resolvido o erro

--- exercicio/ex009/script.js
+++ exercicio/ex009/script.js
@@ -1,11 +1,12 @@
 function analisar(){
-    var data = new Date
-    var ano = data.getFullYear
-    var Nome = document.getElementById('nome')
-    var AnoNascimento = document.getElementById('AnoNascimento')
-    var Sexo = document.getElementsByTagNameNS('sexo')
-    var resultado = getElementById('resultado')
-    if (AnoNascimento.value.length == 0 || AnoNascimento.value > ano || Nome.length == 0){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nome = document.getElementById('nome')
+    var anoNascimento = document.getElementById('anoNascimento')
+    var sexo = document.getElementsByTagName('sexo')
+    var resultado = document.getElementById('resultado')
+
+    if (anoNascimento.value.length == 0 || anoNascimento.value > ano || nome.length == 0){
         window.alert('alguma informação está errada')
     }
 }

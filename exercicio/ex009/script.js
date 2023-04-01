f434e1683867ab120121b977b155848d07e39486
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-04-01 14:27:34 -0300

    .

--- exercicio/ex009/script.js
+++ exercicio/ex009/script.js
@@ -1,21 +1,29 @@
 function analisar(){
-    let data = new Date()
-    let ano = data.getFullYear()
-    let nome = document.getElementById('nome')
-    let anoNascimento = document.getElementById('anoNascimento')
-    let resultado = document.getElementById('resultado')
-    if (anoNascimento.value.length == 0 || anoNascimento.value > ano || nome.length == 0){
-        alert('alguma informação está errada')
-    }else {
-        let sex = document.getElementsByName('sexo')
-        let idade = ano - anoNascimento.value
-        let genero = ''
+    const data = new Date()
+    const ano = data.getFullYear()
+    const nome = document.getElementById('nome')
+    const anoNascimento = document.getElementById('anoNascimento')
+    const resultado = document.getElementById('resultado')
+
+    if (nome.value.length === 0 || anoNascimento.value.length === 0 || anoNascimento.value > ano){
+        alert('Alguma informação está errada')
+        return
+    }
+
+    const sex = document.getElementsByName('sexo')
+    const idade = ano - anoNascimento.value
+    let genero = ''
+    let img = document.createElement('img')
+    img.setAttribute('id','foto')
     
-        if (sex[0].checked){
-            genero = 'um Homem'
-        }else if(sexo[1].checked){
+    if (sex[0].checked){
+        genero = 'um Homem'
+    }else if(sex[1].checked){
         genero = 'uma Mulher'
-        }
-        resultado.innerHTML =`<p> ${nome.value} é ${genero} de ${idade} anos</p>`
-}
+    } else {
+        alert('Por favor, selecione um gênero.')
+        return
+    }
+
+    resultado.innerHTML = `<p>${nome.value} é ${genero} de ${idade} anos</p>`
 }

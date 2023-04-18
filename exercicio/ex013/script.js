fc8c9694adc157f24a04c234756006362f6b5755
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-04-18 20:10:56 -0300

    .

---
+++ exercicio/ex013/script.js
@@ -0,0 +1,42 @@
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade – Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+const totalCategoria = booksByCategory.length
+console.log(totalCategoria)
+for (category of booksByCategory){
+    console.log(category.category)
+    console.log(category.books.length)
+}

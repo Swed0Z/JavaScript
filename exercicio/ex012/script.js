1b6aa54197edba229fa7a6e913f73ef693c4ec92
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-05-29 22:39:28 -0300

    finalizado
    acabei de finalizar o a função resultado.

--- exercicio/ex012/script.js
+++ exercicio/ex012/script.js
@@ -1,9 +1,10 @@
-var numeros = [];
+let numeros = [];
+let resultado = document.getElementById('resultado');
 
 function addNumbers() {
-  const numeroInput = document.getElementById('numero');
-  const numero = parseInt(numeroInput.value);
-  const lista = document.getElementById('lista');
+  let numeroInput = document.getElementById('numero');
+  let numero = parseInt(numeroInput.value);
+  let lista = document.getElementById('lista');
 
   if (isNaN(numero)){
     alert('Você não escolheu um número, digite novamente');
@@ -21,11 +22,31 @@ function addNumbers() {
 
   numeros.push(numero);
   lista.innerHTML += `${numero}<br>`;
+  resultado.innerHTML = "";
 }
 function result() {
-  
+  let maior = Math.max(...numeros);
+  let menor = Math.min(...numeros);
+  let tamanho = numeros.length;
+  let soma = 0;
+
+  for (let i = 0; i < tamanho; i++) {
+    soma += numeros[i];
+  }
+  if (tamanho == 0) {
+    alert('a lista está vazia')
+    return;
+  }
+  let media = soma / tamanho;
+  resultado.innerHTML = "";
+  resultado.innerHTML += `A lista tem ${tamanho} elemento(s).<br>`;
+  resultado.innerHTML += `O maior elemento é ${maior}.<br>`;
+  resultado.innerHTML += `O menor número é ${menor}.<br>`;
+  resultado.innerHTML += `A soma dos elementos é ${soma}.<br>`;
+  resultado.innerHTML += `A média dos elementos é ${media}.<br>`;
 }
 function clean() {
   numeros = [];
   lista.innerHTML = "" ;
+  resultado.innerHTML = "";
 }

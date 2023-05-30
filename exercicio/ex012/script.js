9d0c40f58c5d7daad307f0f321982c7d11ca17b2
Swed0Z <119977510+Swed0Z@users.noreply.github.com>
2023-05-29 21:54:13 -0300

    revolver e adicionar parte 1
    resolvi o problema de NaN quando bloco de número estiver vazio,  adicionar um botão com a função clean e comecei a fazer a função resultado.

--- exercicio/ex012/script.js
+++ exercicio/ex012/script.js
@@ -1,10 +1,14 @@
-const numeros = [];
+var numeros = [];
 
 function addNumbers() {
   const numeroInput = document.getElementById('numero');
   const numero = parseInt(numeroInput.value);
-  const resultado = document.getElementById('resultado');
-  
+  const lista = document.getElementById('lista');
+
+  if (isNaN(numero)){
+    alert('Você não escolheu um número, digite novamente');
+    return;
+  }
   if (numero < 1 || numero > 100) {
     alert(`O número ${numero} está fora do intervalo permitido (1 a 100).`);
     return;
@@ -16,5 +20,12 @@ function addNumbers() {
   }
 
   numeros.push(numero);
-  resultado.innerHTML += `${numero}<br>`;
+  lista.innerHTML += `${numero}<br>`;
+}
+function result() {
+  
+}
+function clean() {
+  numeros = [];
+  lista.innerHTML = "" ;
 }
